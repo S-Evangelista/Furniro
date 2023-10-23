1a3c74c9a87ca500bea2aa962b0bfa712fcdc626
a <stefanystrenner@gmail.com>
2023-10-23 20:28:13 -0300

    Atualizações

--- src/Componentes/Pagina3.jsx
+++ src/Componentes/Pagina3.jsx
@@ -1,32 +1,75 @@
 import "./Pagina3.css"
 
 const Pagina3 = () => {
-    return(
-    <>
-        <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Furniro</title>
-        </head>
-        <body>
-            <p className="tituloP3"> Open our products </p>
-            <div className="gradeP3">
-                <div className="produto">
-                    <img src="./img/1.png"></img>
-                    <p className="textProduto">Syltherine</p>
-                    <p className="subtextP">Stylish cofe chair</p>
-                    <p className="precoP">Rp 2.500.000</p>
-                    <p className="subPrecoP">Rp 3.500.000</p>
+    return (
+        <>
+            <html lang="pt-BR">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>Furniro</title>
+                </head>
+                <body>
+                    <p className="tituloP3"> Open our products </p>
+                    <div className="gradeP3">
+                        <div className="linha">
+                            <div className="produto">
+                                <img src="./img/1.png"></img>
+                                <div className="textoProduto">
+                                    <p className="tituloProduto">Syltherine</p>
+                                    <p className="subtituloP">Stylish cofe chair</p>
+                                </div>
+                                <div className="preco">
+                                    <p className="precoP">Rp 2.500.000</p>
+                                    <p className="subPrecoP">Rp 3.500.000</p>
+                                </div>
 
-                </div>
+                            </div>
+                            <div className="produto">
+                                <img src="./img/1.png"></img>
+                                <div className="textoProduto">
+                                    <p className="tituloProduto">Syltherine</p>
+                                    <p className="subtituloP">Stylish cofe chair</p>
+                                </div>
+                                <div className="preco">
+                                    <p className="precoP">Rp 2.500.000</p>
+                                    <p className="subPrecoP">Rp 3.500.000</p>
+                                </div>
 
-            </div>
-            
-        </body>
-        </html>
+                            </div>
+                            <div className="produto">
+                                <img src="./img/1.png"></img>
+                                <div className="textoProduto">
+                                    <p className="tituloProduto">Syltherine</p>
+                                    <p className="subtituloP">Stylish cofe chair</p>
+                                </div>
+                                <div className="preco">
+                                    <p className="precoP">Rp 2.500.000</p>
+                                    <p className="subPrecoP">Rp 3.500.000</p>
+                                </div>
 
-    </>
+                            </div>
+                            <div className="produto">
+                                <img src="./img/1.png"></img>
+                                <div className="textoProduto">
+                                    <p className="tituloProduto">Syltherine</p>
+                                    <p className="subtituloP">Stylish cofe chair</p>
+                                </div>
+                                <div className="preco">
+                                    <p className="precoP">Rp 2.500.000</p>
+                                    <p className="subPrecoP">Rp 3.500.000</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </body>
+            </html>
+
+        </>
     );
 };
 
